093a45a997bda03cb3747473c3cd78902f926d1c
oit <https://github.com/molander80/proba.git>
2022-02-22 17:48:45 +0300

    update

--- tect2.py
+++ tect2.py
@@ -14,9 +14,29 @@ class TopWindow:
         self.Label_1 = Label(self.window, image=self.our_icon_1, bg="White")
         self.label_2 = Label(self.window, image=self.our_icon_2, bg="White")
         self.label_3 = Label(self.window, font=10, bg="White")
+        self.label_4 = Label(self.window, font=10, bg="White")
+        self.label_5 = Label(self.window, font=10, bg="White")
+        self.label_6 = Label(self.window, font=10, bg="White")
+        self.label_7 = Label(self.window, font=10, bg="White")
+        self.label_8 = Label(self.window, font=10, bg="White")
+        self.label_9 = Label(self.window, font=10, bg="White")
+        self.label_10 = Label(self.window, font=10, bg="White")
+        self.label_11 = Label(self.window, font=10, bg="White")
+        self.label_12 = Label(self.window, font=10, bg="White")
+        self.label_13 = Label(self.window, font=10, bg="White")
         self.Label_1.grid(row=0, column=0, sticky=W, padx=5, pady=5)
         self.label_2.grid(row=0, column=4, rowspan=3, columnspan=2)
         self.label_3.grid(row=0, column=0, columnspan=5)
+        self.label_4.grid(row=1, column=0, sticky=W, padx=5, pady=5)
+        self.label_5.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+        self.label_6.grid(row=3, column=0, sticky=W, padx=5, pady=5)
+        self.label_7.grid(row=4, column=0, sticky=W, padx=5, pady=5)
+        self.label_8.grid(row=5, column=0, sticky=W, padx=5, pady=5)
+        self.label_9.grid(row=1, column=3, sticky=W, padx=5, pady=5)
+        self.label_10.grid(row=2, column=3, sticky=W, padx=5, pady=5)
+        self.label_11.grid(row=3, column=3, sticky=W, padx=5, pady=5)
+        self.label_12.grid(row=4, column=3, sticky=W, padx=5, pady=5)
+        self.label_13.grid(row=5, column=3, sticky=W, padx=5, pady=5)
 
     def grad_focus(self):
 
@@ -39,13 +59,12 @@ class WindowFirst(TopWindow):
 
     def draw_label(self):
 
-        Label(self.window, text="Объем спирта:", font=10, bg="White").grid(row=1, column=0, sticky=W, padx=5, pady=5)
-        Label(self.window, text="литр:", font=10, bg="White").grid(row=1, column=3)
-        Label(self.window, text="Процент спирта до:", font=10, bg="White").grid(row=2, column=0, sticky=W, padx=5,
-                                                                                pady=5)
-        Label(self.window, text="%:", font=10, bg="White").grid(row=2, column=3)
-        Label(self.window, text="Процент после: ", font=10, bg="White").grid(row=3, column=0, sticky=W, padx=5, pady=5)
-        Label(self.window, text="%:", font=10, bg="White").grid(row=3, column=3)
+        self.label_4["text"] = "Объем спирта:"
+        self.label_5["text"] = "Процент спирта до:"
+        self.label_6["text"] = "Процент после: "
+        self.label_9["text"] = "литр:"
+        self.label_10["text"] = "°:"
+        self.label_11["text"] = "°:"
 
     def draw_entry(self):
 
@@ -131,7 +150,8 @@ class WindowSecond(TopWindow):
             second_fortress = self.entry_4.get()
             first_liquid, second_liquid = float(first_liquid), float(second_liquid)
             first_fortress, second_fortress = int(first_fortress), int(second_fortress)
-            fortress = (first_liquid * first_fortress + second_liquid * second_fortress) / (first_liquid + second_liquid)
+            fortress = (first_liquid * first_fortress + second_liquid * second_fortress) / \
+                       (first_liquid + second_liquid)
             self.label_3["text"] = "Крепость на выходе: " + str(round(fortress, 2)) + " %."
 
         except ValueError:
@@ -155,38 +175,49 @@ class WindowThree(TopWindow):
         self.entry_1 = Entry(self.window, width=10, bg="White")
         self.entry_2 = Entry(self.window, width=10, bg="White")
         self.entry_3 = Entry(self.window, width=10, bg="White")
+        self.entry_4 = Entry(self.window, width=10, bg="White")
+        self.entry_5 = Entry(self.window, width=10, bg="White")
 
     def draw_label(self):
 
-        Label(self.window, text="Объем спирта:", font=10, bg="White").grid(row=1, column=0, sticky=W, padx=5, pady=5)
-        Label(self.window, text="литр:", font=10, bg="White").grid(row=1, column=3)
-        Label(self.window, text="Крепость спитра-сырца:", font=10, bg="White").\
-            grid(row=2, column=0, sticky=W, padx=5, pady=5)
-        Label(self.window, text="%:", font=10, bg="White").grid(row=2, column=3)
-        Label(self.window, text="Крепость на выходе: ", font=10, bg="White").\
-            grid(row=3, column=0, sticky=W, padx=5, pady=5)
-        Label(self.window, text="%:", font=10, bg="White").grid(row=3, column=3)
+        self.label_4["text"] = "Объем спирта:"
+        self.label_5["text"] = "Крепость спитра-сырца:"
+        self.label_6["text"] = "Крепость на выходе: "
+        self.label_7["text"] = "Доля голов:"
+        self.label_8["text"] = "Доля хвостов"
+        self.label_9["text"] = "литр:"
+        self.label_10["text"] = "°:"
+        self.label_11["text"] = "°:"
+        self.label_12["text"] = "%:"
+        self.label_13["text"] = "%:"
 
     def draw_entry(self):
 
         self.entry_1.grid(row=1, column=1, sticky=EW)
         self.entry_2.grid(row=2, column=1, sticky=EW)
         self.entry_3.grid(row=3, column=1, sticky=EW)
+        self.entry_4.grid(row=4, column=1, sticky=EW)
+        self.entry_5.grid(row=5, column=1, sticky=EW)
 
     def draw_button(self):
 
-        Button(self.window, text="ПОСЧИТАТЬ", command=self.alcohol, bg="White").grid(row=4, column=4, sticky=EW)
-        Button(self.window, text="НАЗАД", bg="White", command=self.window.destroy).grid(row=4, column=0, sticky=W)
+        Button(self.window, text="ПОСЧИТАТЬ", command=self.alcohol, bg="White").grid(row=6, column=4, sticky=EW)
+        Button(self.window, text="НАЗАД", bg="White", command=self.window.destroy).grid(row=6, column=0, sticky=W)
 
     def alcohol(self):
 
         try:
             self.label_2.configure(image=self.our_icon_3)
-            p = self.entry_1.get()
-            n = self.entry_2.get()
-            m = self.entry_3.get()
-            n, m = int(n), int(m)
-            self.label_3["text"] = round(m, 2)
+            amount_of_alcohol = self.entry_1.get()
+            alcohol_strength = self.entry_2.get()
+            percentage_of_alcohol = self.entry_3.get()
+            amount_of_alcohol = float(amount_of_alcohol)
+            alcohol_strength = int(alcohol_strength)
+            percentage_of_alcohol = int(percentage_of_alcohol)
+            pure_alcohol = amount_of_alcohol * alcohol_strength / 100
+            volume_of_heads = pure_alcohol * percentage_of_alcohol * 10
+            self.label_3["text"] = "Объем чистого спирта:  " + str(round(pure_alcohol, 2)) + " лит." + \
+                                   "\n Объем «голов»:  " + str(round(volume_of_heads, 2)) + " милилит."
 
         except ValueError:
             self.label_3["text"] = "Введите корректное значение"
